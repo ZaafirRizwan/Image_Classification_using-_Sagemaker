55a48aef7119b1eab47a5770f06d84d2becd6522
Zaafir Rizwan <zaafir.rizwan@gmail.com>
2023-02-02 19:04:21 +0000

    commit

--- .ipynb_checkpoints/train_model-checkpoint.py
+++ .ipynb_checkpoints/train_model-checkpoint.py
@@ -13,6 +13,9 @@ import tempfile
 from PIL import Image
 from PIL import ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True
+import os
+import logging
+import sys
 
 import argparse
 
@@ -24,74 +27,25 @@ import smdebug.pytorch as smd
 
 #  For Debugging
 import smdebug.pytorch as smd
+import subprocess
 
-##############################Dataset Class##################################
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler(sys.stdout))
 
-# source = https://stackoverflow.com/questions/54003052/how-do-i-implement-a-pytorch-dataset-for-use-with-aws-sagemaker
-
-class ImageDataset(Dataset):
-    def __init__(self, path , Train=True, transform = None):
-        self.path = path
-        self.s3 = boto3.resource('s3')
-        self.bucket = self.s3.Bucket(path)
-        if Train:
-            self.files = [obj.key for obj in self.bucket.objects.all() if "train" in obj.key and ".jpg" in obj.key]
-        else:
-            self.files = [obj.key for obj in self.bucket.objects.all() if "test" in obj.key and ".jpg" in obj.key]  
-        
-        self.transform = transform
-        if transform is None:
-            self.transform = transforms.Compose([
-                transforms.Resize((224, 224)),
-                transforms.ToTensor(),
-                transforms.Normalize(mean = [0.485, 0.456, 0.406],
-                                    std = [0.229, 0.224, 0.225])
-            ])
-        
-        self.labels = {}
-        for i in self.files:
-            if i not in self.labels.keys():
-                self.labels[i.split('/')[1]]  =  int(i.split('/')[1][0:3]) - 1
-
-    def __len__(self):
-        return len(self.files)
-
-    def __getitem__(self, idx):
-        img_name = self.files[idx]
-
-        # we may infer the label from the filename
-
-        mylabel = img_name.split('/')[1]
-
-        # we need to download the file from S3 to a temporary file locally
-        # we need to create the local file name
-        obj = self.bucket.Object(img_name)
-        tmp = tempfile.NamedTemporaryFile()
-        tmp_name = '{}.jpg'.format(tmp.name)
-
-        # now we can actually download from S3 to a local place
-        with open(tmp_name, 'wb') as f:
-            obj.download_fileobj(f)
-            f.flush()
-            f.close()
-            image = Image.open(tmp_name)
-
-        if self.transform:
-            image = self.transform(image)
-
-        # imagelabel = torch.zeros(133)
-        # imagelabel[self.labels[mylabel]] = 1
-        imagelabel = self.labels[mylabel]
-            
-        return image, imagelabel
-    
-    
-##############################-------------##################################
 
 
 s3 = boto3.client('s3')
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+# Define the S3 bucket and local directory
+bucket_name = 'myimageclassificationbucket'
+local_dir = './'
+
+# Construct the sync command as a list of arguments
+cmd = ['aws', 's3', 'sync', 's3://' + bucket_name, local_dir]
+subprocess.run(cmd,stdout=subprocess. DEVNULL)
 
 
 def test(model, test_loader,criterion,hook):
@@ -109,6 +63,10 @@ def test(model, test_loader,criterion,hook):
     
     # Setting SMDEBUG hook for testing Phase
     hook.set_mode(smd.modes.EVAL)
+    
+    if hook:
+        hook.register_loss(criterion)
+    
     test_loss = 0
     correct = 0
     loss = 0
@@ -118,14 +76,14 @@ def test(model, test_loader,criterion,hook):
             target = target.to(device)
             output = model(data)
             loss = criterion(output,target)
-            test_loss += loss.sum().item() # sum up batch loss
+            test_loss += loss.item() # sum up batch loss
             pred = output.argmax(dim=1,keepdim=True) # get the index of the max log-probability
             correct += pred.eq(target.view_as(pred)).sum().item()
             
     test_loss /= len(test_loader.dataset)
     
-    print(
-        "\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n".format(
+    logger.info(
+        "Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n".format(
             test_loss, correct, len(test_loader.dataset), 100.0 * correct / len(test_loader.dataset)
         )
     )
@@ -146,23 +104,27 @@ def train(model, train_loader, criterion, optimizer, epoch,hook):
         None
     '''
     
-    if hook:
-        hook.register_loss(optimizer)
+    model.fc.require_grad = True
     
     # Setting SMDEBUG hook for model training loop
     hook.set_mode(smd.modes.TRAIN)
+    
+    if hook:
+        hook.register_loss(criterion)
+    
+    
+    
     for batch_idx, (data, target) in enumerate(train_loader):
-        model_ft = model.to(device)
         data = data.to(device)
         target = target.to(device)
         optimizer.zero_grad()
-        output = model_ft(data)
+        output = model(data)
         loss = criterion(output, target)
         loss.backward()
         optimizer.step()
         if batch_idx % 100 == 0:
-            print(
-                "Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}".format(
+            logger.info(
+                "Train Epoch: {} [{}/{} ({:.0f}%)] Loss: {:.6f}".format(
                     epoch,
                     batch_idx * len(data),
                     len(train_loader.dataset),
@@ -210,8 +172,9 @@ def create_data_loaders(data, batch_size):
     Returns:
         Dataloader i.e Train and test
     '''
-    
+    logger.info("Get train data loader")
     train_dataset_loader = torch.utils.data.DataLoader(data["train"], batch_size=batch_size, shuffle=True, num_workers=1)
+    logger.info("Get test data loader")
     test_dataset_loader  = torch.utils.data.DataLoader(data["test"] , batch_size=batch_size, shuffle=False,num_workers=1)
     dataloaders = {'train': train_dataset_loader, 'test': test_dataset_loader}
     
@@ -224,17 +187,41 @@ def main(args):
     
     ##################Debugging###################
     # Registering SMDEBUG hook to save output tensors.
-    hook=smd.get_hook(create_if_not_exists=True)
+    hook = smd.get_hook(create_if_not_exists=True)
     hook.register_hook(model)
     ##############################################
     
     # Creating Loss Function and optimizer
     loss_criterion = nn.CrossEntropyLoss(reduction='sum')
-    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=0.9)
+    optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
+    
+    train_transform = transforms.Compose([
+                transforms.Resize((224, 224)),
+                transforms.RandomHorizontalFlip(),
+                transforms.RandomRotation(10),
+                transforms.RandomResizedCrop(size=224,scale=(0.8, 1.0)),
+                transforms.ToTensor(),
+                transforms.Normalize(mean = [0.485, 0.456, 0.406],
+                                    std = [0.229, 0.224, 0.225])
+            ])
+
+    test_transform = transforms.Compose([
+                    transforms.Resize((224, 224)),
+                    transforms.ToTensor(),
+                    transforms.Normalize(mean = [0.485, 0.456, 0.406],
+                                        std = [0.229, 0.224, 0.225])
+                ])
     
     
-    train_dataset = ImageDataset("myimageclassificationbucket",Train=True)
-    test_dataset = ImageDataset("myimageclassificationbucket",Train=False)
+    
+    
+    if hook:
+        hook.register_loss(loss_criterion)
+            
+    
+    train_dataset = torchvision.datasets.ImageFolder(root="./train", transform = train_transform)
+    test_dataset = torchvision.datasets.ImageFolder(root="./test", transform = test_transform)
+    
     
     data = {"train":train_dataset, "test":test_dataset}
     
@@ -247,6 +234,7 @@ def main(args):
     for epoch in range(args.epochs):
         train(model, dataloaders['train'], loss_criterion, optimizer, epoch, hook)
         test(model, dataloaders['test'], loss_criterion, hook)
+        # save_model(model, args.model_dir)
     
     '''
     TODO: Save the trained model
@@ -274,7 +262,7 @@ if __name__=='__main__':
                    metavar="LR",
                    help="learning rate (default: 1.0)",
                    )
-    
+        
     args = parser.parse_args()
     
     #  Printing Arguments
